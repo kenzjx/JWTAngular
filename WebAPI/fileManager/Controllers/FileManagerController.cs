82a8f23ded6399c2c22be550bf4b79d75d0f5fc8
phong <phong@pop-os.localdomain>
2022-05-20 15:16:55 +0700

    UploadFile

--- WebAPI/fileManager/Controllers/FileManagerController.cs
+++ WebAPI/fileManager/Controllers/FileManagerController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.IO.Compression;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -132,5 +133,56 @@ namespace fileManager.Controllers
 
             return File(memory, contentType, fileName);
         }
+          public (string fileType, byte[] archiveData, string archiveName) DownloadFiles(string subDirectory)  
+        {  
+            var zipName = $"archive-{DateTime.Now.ToString("yyyy_MM_dd-HH_mm_ss")}.zip";  
+  
+            var files = Directory.GetFiles(Path.Combine(_hostingEnvironment.ContentRootPath, subDirectory)).ToList();  
+  
+            using (var memoryStream = new MemoryStream())  
+            {  
+                using (var archive = new ZipArchive(memoryStream, ZipArchiveMode.Create, true))  
+                {  
+                    files.ForEach(file =>  
+                    {  
+                        var theFile = archive.CreateEntry(file);  
+                        using (var streamWriter = new StreamWriter(theFile.Open()))  
+                        {  
+                            streamWriter.Write(File.ReadAllText(file));  
+                        }  
+  
+                    });  
+                }  
+  
+                return ("application/zip", memoryStream.ToArray(), zipName);  
+            }  
+  
+        }
+        #region Size Converter  
+        public string SizeConverter(long bytes)  
+        {  
+            var fileSize = new decimal(bytes);  
+            var kilobyte = new decimal(1024);  
+            var megabyte = new decimal(1024 * 1024);  
+            var gigabyte = new decimal(1024 * 1024 * 1024);  
+  
+            switch (fileSize)  
+            {  
+                case var _ when fileSize < kilobyte:  
+                    return $"Less then 1KB";  
+                case var _ when fileSize < megabyte:  
+                    return $"{Math.Round(fileSize / kilobyte, 0, MidpointRounding.AwayFromZero):##,###.##}KB";  
+                case var _ when fileSize < gigabyte:  
+                    return $"{Math.Round(fileSize / megabyte, 2, MidpointRounding.AwayFromZero):##,###.##}MB";  
+                case var _ when fileSize >= gigabyte:  
+                    return $"{Math.Round(fileSize / gigabyte, 2, MidpointRounding.AwayFromZero):##,###.##}GB";  
+                default:  
+                    return "n/a";  
+            }  
+        }  
+        #endregion  
+  
+    }  
     }
+
 }
